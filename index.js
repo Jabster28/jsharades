799984272ba691fe7e28c7c315a18fc9a8234d22
Justyn Boyer <29015942+Jabster28@users.noreply.github.com>
2019-03-11 19:15:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const art = require("ascii-art")
-var { Timer } = require("easytimer.js")
+var {
+	Timer
+} = require("easytimer.js")
 const ran = require("randray")
 var r = require("readline")
 pc = 0
@@ -65,36 +67,39 @@ rl.question("Welcome to Charades! Please tell me your player's names, seperated 
 		o[p[i]] = {
 			points: 0
 		}
+	}
 
-		console.log("So you have " + p.length + " people, good. The order will be:\n" + p + "\nLets Play!\n")
-		console.log("I will tell you who is currently in front of the display, and then a word will appear above that person. You will try to act out the object and make the person guess what it is without talking to them. You can, however, make noises. Everytime they get a word right, press enter on the keyboard to move to a different word.")
-		rl.question(p[pc] + " will go first. Once they can't see the display (ideally in front of or adjacent to it), you can press enter and start acting:", function (a) {
-			end = false
-			var timer = new Timer();
-			timer.start({
-				precision: 'seconds',
-				startValues: {
-					seconds: 0
-				},
-				target: {
-					seconds: 5
-				}
-			});
-			timer.addEventListener('targetAchieved', function (e) {
-				end = true
-			});
-			q = function () {
-				art.font(ran(s), "Doom", function (rendered) {
-					rl.question(rendered, function (a) {
-						o[p[pc]].points++
-						if (!(end)) q()
-					})
-				});
+	console.log("So you have " + p.length + " people, good. The order will be:\n" + p + "\nLets Play!\n")
+	console.log("I will tell you who is currently in front of the display, and then a word will appear above that person. You will try to act out the object and make the person guess what it is without talking to them. You can, however, make noises. Everytime they get a word right, press enter on the keyboard to move to a different word.")
+	rl.question(p[pc] + " will go first. Once they can't see the display (ideally in front of or adjacent to it), you can press enter and start acting:", function (a) {
+		end = false
+		var timer = new Timer();
+		timer.start({
+			precision: 'seconds',
+			startValues: {
+				seconds: 0
+			},
+			target: {
+				seconds: 5
 			}
-			q()
+		});
+		timer.addEventListener('targetAchieved', function (e) {
+			end = true
+		});
+		q = function () {
+			art.font(ran(s), "Doom", function (rendered) {
+				rl.question(rendered, function (a) {
+					o[p[pc]].points++
+					if (!(end)) w()
+				})
+			});
+		}
+		q()
+
+		w = function () {
 			if (pc > p.length - 1) pc = 0
 			console.log("Nice! " + p[pc] + " got " + o[p[pc]].points)
-		})
+		}
+	})
 
-	}
 })
