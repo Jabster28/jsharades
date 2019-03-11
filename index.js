3d1922baa064c1241dbd5317075c804a27d71e38
Justyn Boyer <29015942+Jabster28@users.noreply.github.com>
2019-03-11 19:19:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -71,35 +71,41 @@ rl.question("Welcome to Charades! Please tell me your player's names, seperated 
 
 	console.log("So you have " + p.length + " people, good. The order will be:\n" + p + "\nLets Play!\n")
 	console.log("I will tell you who is currently in front of the display, and then a word will appear above that person. You will try to act out the object and make the person guess what it is without talking to them. You can, however, make noises. Everytime they get a word right, press enter on the keyboard to move to a different word.")
-	rl.question(p[pc] + " will go first. Once they can't see the display (ideally in front of or adjacent to it), you can press enter and start acting:", function (a) {
-		end = false
-		var timer = new Timer();
-		timer.start({
-			precision: 'seconds',
-			startValues: {
-				seconds: 0
-			},
-			target: {
-				seconds: 5
-			}
-		});
-		timer.addEventListener('targetAchieved', function (e) {
-			end = true
-		});
-		q = function () {
-			art.font(ran(s), "Doom", function (rendered) {
-				rl.question(rendered, function (a) {
-					o[p[pc]].points++
-					if (!(end)) w()
-				})
+	x = funxtion() {
+		rl.question("It is " + p[pc] + "'s turn. Once they can't see the display (ideally in front or to the right of it), you can press enter and start acting:", function (a) {
+			end = false
+			var timer = new Timer();
+			timer.start({
+				precision: 'seconds',
+				startValues: {
+					seconds: 0
+				},
+				target: {
+					seconds: 30
+				}
 			});
-		}
-		q()
+			timer.addEventListener('targetAchieved', function (e) {
+				end = true
+			});
+			q = function () {
+				art.font(ran(s), "Doom", function (rendered) {
+					rl.question(rendered, function (a) {
+						o[p[pc]].points++
+						if (end) w()
+					})
+				});
+			}
+			q()
 
-		w = function () {
-			if (pc > p.length - 1) pc = 0
-			console.log("Nice! " + p[pc] + " got " + o[p[pc]].points)
-		}
-	})
+			w = function () {
+				if (pc > p.length - 1) pc = 0
+				console.log("Nice! " + p[pc] + " got " + o[p[pc]].points)
+				console.log("\nNext person!\n\n")
+				x()
+			}
+		})
+	}
+	console.log("\n\n")
+	x()
 
 })
