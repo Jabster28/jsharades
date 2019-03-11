9642d8e45a2d0192196ba3faef27808bbf5df69f
Jabster28 <justynboyer@gmail.com>
2019-03-11 19:23:51 +0000

    Fixed error

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ rl.question("Welcome to Charades! Please tell me your player's names, seperated 
 
 	console.log("So you have " + p.length + " people, good. The order will be:\n" + p + "\nLets Play!\n")
 	console.log("I will tell you who is currently in front of the display, and then a word will appear above that person. You will try to act out the object and make the person guess what it is without talking to them. You can, however, make noises. Everytime they get a word right, press enter on the keyboard to move to a different word.")
-	x = funxtion() {
+	x = function() {
 		rl.question("It is " + p[pc] + "'s turn. Once they can't see the display (ideally in front or to the right of it), you can press enter and start acting:", function (a) {
 			end = false
 			var timer = new Timer();
